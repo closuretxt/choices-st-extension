d8b33dc81068753e4be62125fe34a7dba0b3d4f2
closure <126289616+closuretext@users.noreply.github.com>
2026-08-27 14:11:51 -0300

    - Defaults

--- settings/defaultOptions.js
+++ settings/defaultOptions.js
@@ -55,6 +55,7 @@ export const defaultInputPlaceholder = "(empty - assume what the user most likel
 export const defaultInputPrompt = `<input_bar_draft>
 {{input}}
 </input_bar_draft>
+If input is present, it's absolute and should be contained in every option you suggest, someway or the other.
 
 Generate the suggestions now. Your responses shall not provide a ending to the story and only contain actions from user.`;
 
